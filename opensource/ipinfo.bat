913dae4f1d6519096bade89a4636af5bb461bdc9
MRBRONAK <106496490+MRBRONAK@users.noreply.github.com>
2022-05-30 09:27:50 -0400

    Update ipinfo.bat

--- opensource/ipinfo.bat
+++ opensource/ipinfo.bat
@@ -1,1 +1,59 @@
-
+@echo off
+:ipinfo
+setlocal ENABLEDELAYEDEXPANSION
+set webclient=webclient
+if exist "%temp%\%webclient%.vbs" del "%temp%\%webclient%.vbs" /f /q /s >nul
+if exist "%temp%\response.txt" del "%temp%\response.txt" /f /q /s >nul
+echo sUrl = "http://ipinfo.io/json" > %temp%\%webclient%.vbs & goto localip
+:localip
+echo set oHTTP = CreateObject("MSXML2.ServerXMLHTTP.6.0") >> %temp%\%webclient%.vbs
+echo oHTTP.open "GET", sUrl,false >> %temp%\%webclient%.vbs
+echo oHTTP.setRequestHeader "Content-Type", "application/x-www-form-urlencoded" >> %temp%\%webclient%.vbs
+echo oHTTP.setRequestHeader "Content-Length", Len(sRequest) >> %temp%\%webclient%.vbs
+echo oHTTP.send sRequest >> %temp%\%webclient%.vbs
+echo HTTPGET = oHTTP.responseText >> %temp%\%webclient%.vbs
+echo strDirectory = "%temp%\response.txt" >> %temp%\%webclient%.vbs
+echo set objFSO = CreateObject("Scripting.FileSystemObject") >> %temp%\%webclient%.vbs
+echo set objFile = objFSO.CreateTextFile(strDirectory) >> %temp%\%webclient%.vbs
+echo objFile.Write(HTTPGET) >> %temp%\%webclient%.vbs
+echo objFile.Close >> %temp%\%webclient%.vbs
+echo Wscript.Quit >> %temp%\%webclient%.vbs
+start %temp%\%webclient%.vbs
+set /a requests=0
+rem echo Waiting for API response. . .
+echo.
+echo  Looking up IP Address. . .
+echo.
+:checkresponseexists
+set /a requests=%requests% + 1
+if %requests% gtr 7 goto failed
+IF EXIST "%temp%\response.txt" (
+goto response_exist
+) ELSE (
+ping 127.0.0.1 -n 2 -w 1000 >nul
+goto checkresponseexists
+)
+:failed
+taskkill /f /im wscript.exe >nul
+del "%temp%\%webclient%.vbs" /f /q /s >nul
+goto :ipinfo
+:response_exist
+for /f "delims=     " %%i in ('findstr /i "," %temp%\response.txt') do (
+    set data=%%i
+    set data=!data:,=!
+    set data=!data:""=Not Listed!
+    set data=!data:"=!
+    set data=!data:ip:=IP:      !
+    set data=!data:hostname:=Hostname:  !
+    set data=!data:org:=ISP:        !
+    set data=!data:city:=City:      !
+    set data=!data:region:=State:   !
+    set data=!data:country:=Country:    !
+    set data=!data:postal:=Postal:  !
+    set data=!data:loc:=Location:   !
+    set data=!data:timezone:=Timezone:  !
+    echo !data!
+)
+echo.
+del "%temp%\%webclient%.vbs" /f /q /s >nul
+del "%temp%\response.txt" /f /q /s >nul
